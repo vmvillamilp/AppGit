457fa2df835b323208cabd2e973116456929032e
Victor Villamil <vmvillamilp@correo.udistrital.edu.co>
2018-11-02 07:20:35 -0500

    sixth push

--- CEAPP/src/app/app.component.ts
+++ CEAPP/src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component, ViewChild } from '@angular/core';
+ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { LoginPage } from '../pages/login/login';
 import { ListPage } from '../pages/list/list';
 
 @Component({
@@ -13,7 +12,7 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = LoginPage;
+  rootPage: any = HomePage;
 
   pages: Array<{title: string, component: any}>;
 
