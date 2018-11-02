e6c833e81c996158ec96a8f16b478b2a5c62e094
Victor Villamil <vmvillamilp@correo.udistrital.edu.co>
2018-11-02 08:15:27 -0500

    fifth commit

--- CEAPP/src/app/app.module.ts
+++ CEAPP/src/app/app.module.ts
@@ -5,6 +5,11 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { ContratoPage } from '../pages/contrato/contrato';
+import { ProyectoPage } from '../pages/proyecto/proyecto';
+import { RegistroPage } from '../pages/registro/registro';
+import { SubastaPage } from '../pages/subasta/subasta';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,7 +18,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LoginPage,
+    ContratoPage,
+    ProyectoPage,
+    RegistroPage,
+    SubastaPage
   ],
   imports: [
     BrowserModule,
@@ -23,7 +33,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LoginPage,
+    ContratoPage,
+    ProyectoPage,
+    RegistroPage,
+    SubastaPage
   ],
   providers: [
     StatusBar,
